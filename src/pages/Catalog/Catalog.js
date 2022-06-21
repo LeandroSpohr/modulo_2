eccd9c9fda4efeaa61e6246469ba407865cbad14
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-06-20 22:30:41 -0300

    list movies

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,13 +1,30 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import * as ApiTmbService from '../../services/apiTmdb'
 
-function Catalog () {
+function Catalog() {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  const imgUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2'
+
   useEffect(() => {
     ApiTmbService.getPolular()
-      .then((response) => console.log(response))
+      .then((response) => setPopularMovies(response.results))
   }, [])
 
-  return <h1>Catalogo</h1>
+  return <>
+    <h1>Catalogo</h1>
+    <div>
+      <h2>Populares</h2>
+      {
+        popularMovies.map((popularMovie) => {
+          return <>
+            <h5>{popularMovie.title}</h5>
+            <img src={imgUrl + popularMovie.poster_path} alt={popularMovie.title} />
+          </>
+        })
+      }
+    </div>
+  </>
 }
 
 export default Catalog
