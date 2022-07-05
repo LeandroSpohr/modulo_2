afe7c583cf80a65f9b98bba459a0ebb11abb5e01
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-07-04 21:00:24 -0300

    add first component

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,10 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import * as ApiTmbService from '../../services/apiTmdb'
+import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from 'react-icons/md'
+import Title from '../../components/atoms/Title'
 
 function Catalog() {
   const [popularMovies, setPopularMovies] = useState([])
+  const listaRef = useRef()
 
-  const imgUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2'
+  const imgUrl = 'https://image.tmdb.org/t/p/w300'
+
+  function scroll(x) {
+    listaRef.current.scrollTo({
+      left: x,
+      behavior: 'smooth',
+    })
+  }
 
   useEffect(() => {
     ApiTmbService.getPolular()
@@ -13,16 +23,34 @@ function Catalog() {
 
   return <>
     <h1>Catalogo</h1>
-    <div>
-      <h2>Populares</h2>
-      {
-        popularMovies.map((popularMovie) => {
-          return <>
-            <h5>{popularMovie.title}</h5>
-            <img src={imgUrl + popularMovie.poster_path} alt={popularMovie.title} />
-          </>
-        })
-      }
+    <div className='categoria'>
+      <h2 className='categoria-titulo'>Populares</h2>
+      <Title>Populares</Title>
+      <Title>teste</Title>
+      <div
+        className='categoria-lista'
+        ref={listaRef}
+      >
+        <button
+          className='botao-lista categoria-esquerda'
+          onClick={() => scroll(-500)}
+        >
+          <MdOutlineArrowBackIosNew className='icone' size={35} />
+        </button>
+        {
+          popularMovies.map((popularMovie) => {
+            return <>
+              <img src={imgUrl + popularMovie.poster_path} alt={popularMovie.title} />
+            </>
+          })
+        }
+        <button
+          className='botao-lista categoria-direita'
+          onClick={() => scroll(500)}
+        >
+          <MdOutlineArrowForwardIos className='icone' size={35} />
+        </button>
+      </div>
     </div>
   </>
 }
